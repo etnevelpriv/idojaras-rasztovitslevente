f3529f83bcf6c544a339a6be4a3b7ebb06de6744
etnevelpriv <leventerastovits@gmail.com>
2026-02-13 14:03:49 +0100

    fajlbairas kesz

--- Napiidojaras.js
+++ Napiidojaras.js
@@ -1,11 +1,23 @@
 export class Napiidojaras {
-    constructor (aHetNapja, napiMaxHomerseklet, napiMinHomerseklet, idojarasSzovegesen) {
+    constructor(aHetNapja, napiMaxHomerseklet, napiMinHomerseklet, idojarasSzovegesen) {
+        if (typeof aHetNapja != "number" || aHetNapja < 0 || aHetNapja > 6 || aHetNapja == null || aHetNapja == undefined || aHetNapja == NaN) {
+            throw new Error(`A hét napja nincs megvelelően megadva: ${aHetNapja}`);
+        };
+        if (typeof napiMaxHomerseklet != "number" || napiMaxHomerseklet < -50 || napiMaxHomerseklet > 100 || napiMaxHomerseklet == null || napiMaxHomerseklet == undefined || napiMaxHomerseklet == NaN) {
+            throw new Error(`A maximum hőmérsékelt nincs megvelelően megadva: ${napiMaxHomerseklet}`);
+        };
+        if (typeof napiMinHomerseklet != "number" || napiMinHomerseklet < -50 || napiMinHomerseklet > 100 || napiMinHomerseklet == null || napiMinHomerseklet == undefined || napiMinHomerseklet == NaN) {
+            throw new Error(`A maximum hőmérsékelt nincs megvelelően megadva: ${napiMinHomerseklet}`);
+        };
+        if (typeof idojarasSzovegesen != "string" || idojarasSzovegesen == null || idojarasSzovegesen == undefined) {
+            throw new Error(`A maximum hőmérsékelt nincs megvelelően megadva: ${napimanapiMaxHomerseklet}`);
+        };
         this.aHetNapja = aHetNapja;
         this.napiMaxHomerseklet = napiMaxHomerseklet;
         this.napiMinHomerseklet = napiMinHomerseklet;
         this.idojarasSzovegesen = idojarasSzovegesen;
     };
-    toString () {
+    toString() {
         return (`A hét hanyadik napja: ${this.aHetNapja}, Napi maximum hőmérséklet: ${this.napiMaxHomerseklet}, Napi minimum hőmérséklet: ${this.napiMinHomerseklet}, Időjárás típusa: ${this.idojarasSzovegesen}`);
     };
 };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { Napiidojaras } from "./Napiidojaras.js";
 import { beolvas, mainIdojaras } from "./idojaraskezeles.js";
+import fs from "node:fs";
 
 console.log(new Napiidojaras(Number(5), Number(10), Number(-3), "Napos").toString());
 
@@ -7,7 +8,33 @@ const init = async function () {
     const arr = await beolvas();
     const elem = await mainIdojaras();
     arr.push(elem);
-    console.log(arr)
+    // console.log(arr);
+    let legmelegebbNap = arr[0];
+    arr.forEach((element) => {
+        if (element.napiMaxHomerseklet > legmelegebbNap.napiMaxHomerseklet) {
+            legmelegebbNap = element;
+        };
+    });
+    console.log(`A legmelegebb nap: ${legmelegebbNap}`);
+    ujFajlbaIras(arr)
+};
+
+const ujFajlbaIras = function (arr) {
+    let str = `nap;min;max;időjárás\n`;
+    // console.log(arr)
+    try {
+        arr.forEach((element) => {
+            const nap = element.aHetNapja;
+            const max = element.napiMaxHomerseklet;
+            const min = element.napiMinHomerseklet;
+            const type = element.idojarasSzovegesen;
+            str += `${nap};${min};${max};${type}\n`;
+        });
+        // console.log(str)
+        fs.writeFileSync("idojaras-uj.csv", str);
+    } catch (err) {
+        throw new Error(err);
+    };
 };
 
 init();
