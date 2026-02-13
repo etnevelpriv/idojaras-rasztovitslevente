94cb300b55ec9a318258a6cdb100107008cb6fd3
etnevelpriv <leventerastovits@gmail.com>
2026-02-13 13:23:31 +0100

    idojaraskezeles mukodik. promise meg nem

---
+++ idojaraskezeles.js
@@ -0,0 +1,39 @@
+import readline from 'readline';
+import { Napiidojaras } from './Napiidojaras.js';
+
+
+
+const mainIdojaras = function () {
+    const idojarasPromise = new Promise((resolve) => {
+
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        rl.question('Mi a mai max hőmérséklet? ', (max) => {
+            console.log(`Max: ${max}!`);
+            rl.question('Mi a mai minimum hőmérséklet? ', (min) => {
+                console.log(`Min: ${min}`);
+                rl.question('Milyen az időjárás típusa? ', (type) => {
+                    console.log(`Tipus: ${type}`);
+                    const day = new Date();
+                    resolve(new Napiidojaras(Number(day), Number(max), Number(min), type));
+                    rl.close();
+                });
+            });
+        });
+
+        rl.on('close', () => {
+            process.exit(0);
+        });
+    });
+    return idojarasPromise;
+};
+
+const init = async function () {
+    const idojaras = await mainIdojaras();
+    console.log(idojaras.toString())
+};
+
+init();
